9b6a8c3d2415de805da93248eadaf3ceba3493ec
Mohsine Bakhcha <mohsine.52.work@gmail.com>
2023-09-01 11:31:48 +0100

    new version

--- assets/projects.js
+++ assets/projects.js
@@ -1,79 +1,69 @@
-const projects = [
-    {
-        category: "Programming",
-        image: "../img/Chef Portfolio img.png",
-        title: "Chef Portfolio",
-        tag: "",
-        modalurl: "case-study-chef-portfolio.html",
-        id: 1
-    },
-    {
-        category: "Programming",
-        image: "../img/Tiler Portfolio img.png",
-        title: "Tiler Portfolio",
-        tag: "",
-        modalurl: "case-study-chef-portfolio.html",
-        id: 2
-    },
-    {
-        category: "Electricity",
-        image: "../img/SE.png",
-        title: "motor control circuit",
-        tag: "",
-        modalurl: "case-study-1.html",
-        id: 3
-    },
-];
+document.addEventListener('DOMContentLoaded', function() {
+    const software_projects = [
+        {
+            "id": 1,
+            "image_url": "#",
+            "page_url": "#",
+            "tag": "#",
+            "title": "#"
+        },
+    ];
 
-function loadProjects(category) {
-    const projectsContainer = document.querySelector('#projects-container');
-    projectsContainer.innerHTML = ''; 
+    const electricity_projects = [
+        {
+            "id": 1,
+            "image_url": "#",
+            "page_url": "#",
+            "tag": "#",
+            "title": "#"
+        },
 
-    for (let project of projects) {
-        if (project.category === category) {
-            let projectCard = `
-            <div class="project-card">
-                <div class="img-container"><img class="project-image" src="${project.image}"></div>
-                <div class="txt-project">
-                    <h3 class="project-title">${project.title}</h3>
-                    <p>${project.tag}</p>
-                    <button class="btn" onclick="openModal('${project.modalurl}')">
-                        Case Study
-                    </button>
-                </div>
-            </div>`;
-            projectsContainer.insertAdjacentHTML('beforeend', projectCard);
-        }
-    }
+    ];
 
-    $(document).ready(function(){
-        $('.projects-carousel').slick({
-          dots: true,
-          arrows: true,
-          infinite: true,
-          speed: 800,
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                arrows: false,
-                slidesToShow: 1
-              }
-            }
-          ]
+
+    const swiper = new Swiper('#swiper', {
+        loop: true,
+        effect: 'fade',
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        keyboard: {
+            enabled: true
+        },
+        autoplay: {
+            delay: 5000
+        },
+        
+    });
+
+    const populateProjects = (projects) => {
+        let slides = '';
+        projects.forEach((project) => {
+            slides += `<div class="swiper-slide">
+                         <img src="${project.image_url}" />
+                         <div class="slider-info">
+                            <h3>${project.title}</h3>
+                            <p>${project.tag}</p>
+                            <a href="${project.page_url}">Case Study</a>
+                         </div>
+                       </div>`;
         });
-    });           
-}
+        document.querySelector('.swiper-wrapper').innerHTML = slides;
+        swiper.update();
+    };
 
-loadProjects('Programming');
+    populateProjects(software_projects); // Populate with software projects by default
 
-document.querySelectorAll('.category-btn').forEach(button => {
-    button.addEventListener('click', (e) => {
-        document.querySelector('.active').classList.remove('active');
-        e.target.classList.add('active');
-        $('#projects-container').slick('unslick'); 
-        loadProjects(e.target.dataset.category);
+    document.getElementById('softwareBtn').addEventListener('click', function() {
+        populateProjects(software_projects);
+        this.classList.add('active');
+        document.getElementById('electricityBtn').classList.remove('active');
+    });
+
+    document.getElementById('electricityBtn').addEventListener('click', function() {
+        populateProjects(electricity_projects);
+        this.classList.add('active');
+        document.getElementById('softwareBtn').classList.remove('active');
     });
 });
--- assets/script.js
+++ assets/script.js
@@ -5,52 +5,126 @@ window.addEventListener("load", function() {
   setTimeout(function() {
     bodyScroll.style.overflowY = "scroll";
     loading.style.display = "none";
-  }, 1000);
+  }, 3000);
 });
 
-var overlayModal = document.getElementsByClassName("overlay-modal")[0];
-function openModal(filename) {
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-      document.getElementById('modal-body').innerHTML = this.responseText;
-      var modal = document.getElementById("myModal");
-      overlayModal.style.display = "block";
-      modal.style.display = "block";
-      setTimeout(function(){ modal.style.opacity = "1"; }, 50);
-      document.body.style.overflow = 'hidden';
-  }
-  
-  xhr.open('GET', './modals/' + filename, true);
-  xhr.send();
-}
 
-var span = document.getElementsByClassName("close")[0];
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    const startPosition = window.pageYOffset;
+    const targetPosition = target.getBoundingClientRect().top + window.pageYOffset;
+    let startTime = null;
+    const duration = 1000; // 2 seconds
 
-span.onclick = function() {
-  var modal = document.getElementById("myModal");
-  modal.style.opacity = "0";
-  overlayModal.style.display = "none";
-  setTimeout(function(){ modal.style.display = "none"; }, 500);
-  document.body.style.overflow = 'auto';
-}
+    function animateScroll(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const progress = Math.min(timeElapsed / duration, 1);
+      window.scrollTo(0, startPosition + (targetPosition - startPosition) * progress);
 
-var modal = document.getElementById("myModal");
-window.onclick = function(event) {
-  if (event.target == overlayModal) {
-    overlayModal.style.display = "none";
-    modal.style.opacity = "0";
-    setTimeout(function(){ modal.style.display = "none"; }, 500);
-    document.body.style.overflow = 'auto';
-  }
-}
+      if (timeElapsed < duration) {
+        window.requestAnimationFrame(animateScroll);
+      }
+    }
 
-
-document.querySelectorAll('a[href^="#"]').forEach (anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+    window.requestAnimationFrame(animateScroll);
   });
 });
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const videoSection = document.getElementById('video-section');
+  const backgroundVideo = document.getElementById('background-video');
+
+  if (videoSection && backgroundVideo) {
+    window.addEventListener('scroll', () => {
+      const sectionTop = videoSection.offsetTop;
+      const sectionBottom = sectionTop + videoSection.offsetHeight;
+      const viewportBottom = window.scrollY + window.innerHeight;
+      
+      if (window.scrollY >= sectionTop && viewportBottom <= sectionBottom) {
+        backgroundVideo.style.position = 'fixed';
+        backgroundVideo.style.top = '0';
+      } else {
+        backgroundVideo.style.position = 'absolute';
+        
+        if (window.scrollY < sectionTop) {
+          backgroundVideo.style.top = '0';
+        } else if (viewportBottom >= sectionBottom) {
+          backgroundVideo.style.top = `${videoSection.offsetHeight - window.innerHeight}px`;
+        }
+      }
+    });
+  } else {
+    console.error('Element not found. Please check your IDs.');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('binary-container');
+  
+  for (let row = 0; row < 54; row++) {
+    for (let col = 0; col < 96; col++) {
+      const binaryDigit = document.createElement('div');
+      binaryDigit.classList.add('binary-digit');
+      
+      // Initial random 0 or 1
+      binaryDigit.innerText = Math.floor(Math.random() * 2).toString();
+      
+      // Positioning
+      binaryDigit.style.top = `${row * 20}px`;
+      binaryDigit.style.left = `${col * 20}px`;
+      
+      // Add to container
+      container.appendChild(binaryDigit);
+
+      // Animate random change
+      setInterval(() => {
+        binaryDigit.innerText = Math.floor(Math.random() * 2).toString();
+      }, Math.random() * 40000 + 5000);  // Random interval between 5000ms and 40000ms
+    }
+  }
+});
+
+
+// Function to initiate the dynamic typing
+function startDynamicTyping() {
+  new TypeIt("#dynamicText", {
+    speed: 75,
+    loop: true,
+    loopDelay: 1000
+  })
+    .pause(300)
+    .type("successful and profitable.", { delay: 2500 })
+    .delete(26)
+    .type("empowering.", { delay: 2500 })
+    .delete(11)
+    .type("lasting.", { delay: 2500 })
+    .delete(8)
+    .type("scalable.", { delay: 2500 })
+    .delete(9)
+    .type("aligned with your vision.", { delay: 2500 })
+    .delete(25)
+    .type("impactful.", { delay: 2500 })
+    .delete(25)
+    .go();
+}
+
+// Type the static part and then start the dynamic part
+new TypeIt("#staticText", {
+  speed: 60
+})
+.type(
+  "It's not just about being a service provider; it's about being a trusted advisor partner. Together, we can build something that is "
+)
+.exec(() => {
+  document.querySelector("#staticText .ti-cursor").style.display = "none";
+  startDynamicTyping();
+})
+.go();
+
